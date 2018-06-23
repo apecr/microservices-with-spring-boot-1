641cbff7adae639b81e2d71c5471b42e3544e9ec
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-06-23 13:08:23 +0200

    Return a JSON response
    [FA-88]

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,18 +1,33 @@
 package com.example.demo;
 
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.domain.Player;
+import com.example.demo.domain.Team;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping("/hi/{name}")
-	public String hiThere(Map<String, String> model, @PathVariable String name) {
-		model.put("name", name);
-		return "hello";
+	private Team team;
+
+	@PostConstruct
+	public void init() {
+		Set<Player> players = new HashSet<Player>();
+		players.add(new Player("Charlie Brown", "pitcher"));
+		players.add(new Player("Snoopy", "shortstop"));
+		team = new Team("Snoop", "California", "Peanuts", players);
+	}
+
+	@RequestMapping("/hi")
+	public @ResponseBody Team hiThere() {
+		return team;
 	}
 
 }
---
+++ src/main/java/com/example/demo/domain/Team.java
@@ -0,0 +1,59 @@
+package com.example.demo.domain;
+
+import java.util.Set;
+
+public class Team {
+
+	private String name;
+
+	private String location;
+
+	private String masscote;
+
+	private Set<Player> players;
+
+	public Team() {
+		super();
+	}
+
+	public Team(String name, String location, String masscote, Set<Player> players) {
+		this();
+		this.name = name;
+		this.location = location;
+		this.masscote = masscote;
+		this.players = players;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getMasscote() {
+		return masscote;
+	}
+
+	public void setMasscote(String masscote) {
+		this.masscote = masscote;
+	}
+
+	public Set<Player> getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(Set<Player> players) {
+		this.players = players;
+	}
+
+}
